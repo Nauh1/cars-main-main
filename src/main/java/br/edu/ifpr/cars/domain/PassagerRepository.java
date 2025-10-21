be3c2af78ed3d453532fc3197e6b5b28611c790f
NauH1 <souzavinicius840@gmail.com>
2025-10-21 14:54:01 -0300

    aa

---
+++ src/main/java/br/edu/ifpr/cars/domain/PassagerRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifpr.cars.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassagerRepository extends JpaRepository<Passager, Long>{
+
+    
+}
