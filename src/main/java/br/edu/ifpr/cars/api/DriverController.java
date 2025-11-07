aad9667db0f40042d8f28a52559826f24b65befb
NauH1 <souzavinicius840@gmail.com>
2025-11-07 11:54:01 -0300

    aas

--- src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import br.edu.ifpr.cars.domain.Driver;
 import br.edu.ifpr.cars.domain.DriverRepository;
+import jakarta.validation.Valid;
 
 @Service
 @RestController
@@ -42,7 +43,7 @@ public class DriverController {
     }
 
     @PostMapping("/drivers")
-    public Driver createDriver(@RequestBody Driver driver) {
+    public Driver createDriver(@RequestBody @Valid Driver driver) {
         return driverRepository.save(driver);
     }
 
