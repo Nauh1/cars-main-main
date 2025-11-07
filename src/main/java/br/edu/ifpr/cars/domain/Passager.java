622a5988d3dd5816f8589c3473efa1b78e0c5577
NauH1 <souzavinicius840@gmail.com>
2025-11-07 11:51:35 -0300

    ---

--- src/main/java/br/edu/ifpr/cars/domain/Passager.java
+++ src/main/java/br/edu/ifpr/cars/domain/Passager.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -14,6 +16,8 @@ public class Passager{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @NotBlank(message = "O nome não pode ser vazio")
+    @Size(min = 3,max = 50)
     String name;
     LocalDate birthDate;
 }
